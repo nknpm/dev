c40750c5feb4ac8ba29e2366a450b1f5b06aea70
Nicholas Ketchum <nketchum@gmail.com>
2015-08-22 22:32:41 -0400

    add protos, restructure testing

--- dev.js
+++ dev.js
@@ -1,7 +1,14 @@
 "use strict";
 
-var utils = {
-  l: console.log
-}
-
 module.exports = utils;
+
+var utils = {}
+
+utils.l = console.log;
+
+utils.protos = function(obj, call) {
+  call(obj);
+  if (obj !== null) {
+    utils.protos(obj.__proto__, call);
+  }
+}
